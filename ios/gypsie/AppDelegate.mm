31fd7c70f1564e7561d569ee187dcae7a7acaf7d
Lim Xuan Hui <limxuanhui1717@gmail.com>
2024-03-07 11:50:38 +0800

    Configuring app delegate files for react-native-app-auth

--- ios/gypsie/AppDelegate.mm
+++ ios/gypsie/AppDelegate.mm
@@ -39,4 +39,15 @@
   return true;
 }
 
+// For deep linking - https://github.com/FormidableLabs/react-native-app-auth/tree/main
+- (BOOL) application: (UIApplication *)application
+             openURL: (NSURL *)url
+             options: (NSDictionary<UIApplicationOpenURLOptionsKey, id> *) options
+{
+  if ([self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:url]) {
+    return YES;
+  }
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
 @end
