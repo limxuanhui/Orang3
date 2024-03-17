567d8cd02592a1b182afb671ee2a4dddcd16d0f3
Lim Xuan Hui <limxuanhui1717@gmail.com>
2024-03-18 03:04:48 +0800

    Modified tsconfig.json and babel.config.js for enabling custom import path

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    "react-native-reanimated/plugin",
+    'react-native-reanimated/plugin',
     [
-      "module:react-native-dotenv",
+      'module:react-native-dotenv',
       {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
         // blocklist: null,
         // allowlist: null,
         // blacklist: null, // DEPRECATED
@@ -15,6 +15,26 @@ module.exports = {
         // safe: false,
         // allowUndefined: true,
         // verbose: false,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        // root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: './src/tests',
+          '@components': './src/components',
+          '@screens': './src/components/screens',
+          '@navigators': './src/components/navigators',
+          '@icons': './src/components/common/icons',
+          '@data': './src/data',
+          '@constants': './src/utils/constants',
+          '@hooks': './src/utils/hooks',
+          '@contexts': './src/utils/contexts',
+          '@helpers': './src/utils/helpers',
+          '@redux': './src/utils/redux',
+        },
       },
     ],
   ],
